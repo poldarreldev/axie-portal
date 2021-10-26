5452049d7c4283fae525813aba9a45e2829dcb73
Darrel Pol <poldarre.dev@gmail.com>
2021-10-25 21:43:42 -0500

    some changes on the file

--- index.js
+++ index.js
@@ -21,23 +21,25 @@ const typeDefs = gql`
 `;
 
 const books = [
-    {
-        title: 'The Awakening',
-        author: 'Darrel Pol',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Jampot Ichiman',
-    },
+  {
+    title: 'The Awakening',
+    author: 'Darrel Pol',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Jampot Ichiman',
+  },
 ];
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-        books: () => books,
-    },
+  Query: {
+    books: () => books,
+  },
 };
+
+
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -45,7 +47,7 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`
+  console.log(`
       🚀  Server is ready at ${url}
       📭  Query at https://studio.apollographql.com/dev
     `);
